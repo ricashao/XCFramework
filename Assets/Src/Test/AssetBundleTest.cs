f48042029bc72d51fc05b6f2e58d2a9ec543c099
ricashao <szc699.good@163.com>
2019-09-29 18:17:45 +0800

    1.添加加载测试代码

---
+++ Assets/Src/Test/AssetBundleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class AssetBundleTest : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 50), "加载login"))
+        {
+            AssetManager.LoadAsset("prefabs/win/win_login.ga", this.LoadAssetComplete);
+        }
+    }
+
+    private void LoadAssetComplete(Object o)
+    {
+        var root = GameObject.Find("Root").transform;
+        (((GameObject)o).transform as RectTransform).SetParent(root.transform as RectTransform,false);
+    }
+}
