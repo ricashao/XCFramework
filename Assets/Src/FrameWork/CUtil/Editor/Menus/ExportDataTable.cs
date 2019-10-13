8f7a82fa13efdec0046d844b71784e8cd5e756c4
ricashao <szc699.good@163.com>
2019-10-14 00:05:59 +0800

    1.开始写配置表生成工具

---
+++ Assets/Src/FrameWork/CUtil/Editor/Menus/ExportDataTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public static class ExportDataTable
+{
+    private static string excelPath = Application.dataPath + "/Excels";
+
+    [MenuItem("Assets/DataTool/Gen Config Table")]
+    public static void onCreateConfigTable()
+    {
+        
+    }
+}
