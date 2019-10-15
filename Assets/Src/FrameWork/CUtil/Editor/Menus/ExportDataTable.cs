534badc63d8b321f2eb972a0ff71179aaa990026
shaozhoucheng <shao.zhou.cheng@163.com>
2019-10-15 08:26:32 +0800

    1.editorwindow编译提示

--- Assets/Src/FrameWork/CUtil/Editor/Menus/ExportDataTable.cs
+++ Assets/Src/FrameWork/CUtil/Editor/Menus/ExportDataTable.cs
@@ -1,43 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-public class ExportDataTable: EditorWindow 
+
+public class ExportDataTable : EditorWindow
 {
     string path;
     Rect rect;
+    public GUIStyle centeredMiniLabel;
+
     [MenuItem("KCFramework/ExcelTool/Gen Data Table")]
     static void Init()
     {
         EditorWindow.GetWindow(typeof(ExportDataTable));
     }
- 
+
+    private void Awake()
+    {
+        centeredMiniLabel = new GUIStyle(EditorStyles.centeredGreyMiniLabel);
+        centeredMiniLabel.normal.textColor = EditorStyles.label.normal.textColor;
+    }
+
     void OnGUI()
     {
-        EditorGUILayout.LabelField("路径");
-        //获得一个长300的框
-        rect = EditorGUILayout.GetControlRect(GUILayout.Width(300));
-        //将上面的框作为文本输入框
-        path = EditorGUI.TextField(rect, path);
-
-        if ((Event.current.type == EventType.DragUpdated)
-            && rect.Contains(Event.current.mousePosition))
+        
+        if (EditorApplication.isCompiling)
         {
-            //改变鼠标的外表
-            DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+            GUILayout.Label("Compiling...", centeredMiniLabel,
+                GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
         }
-
-        //如果鼠标正在拖拽中或拖拽结束时，并且鼠标所在位置在文本输入框内
-        if ((Event.current.type == EventType.DragExited)
-            && rect.Contains(Event.current.mousePosition))
+        else
         {
-            if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+            EditorGUILayout.LabelField("路径");
+            //获得一个长300的框
+            rect = EditorGUILayout.GetControlRect(GUILayout.Width(300));
+            //将上面的框作为文本输入框
+            path = EditorGUI.TextField(rect, path);
+
+            if ((Event.current.type == EventType.DragUpdated)
+                && rect.Contains(Event.current.mousePosition))
             {
-                path = DragAndDrop.paths[0];
-                Debug.Log(path);
+                //改变鼠标的外表
+                DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+            }
+
+            //如果鼠标正在拖拽中或拖拽结束时，并且鼠标所在位置在文本输入框内
+            if ((Event.current.type == EventType.DragExited)
+                && rect.Contains(Event.current.mousePosition))
+            {
+                if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+                {
+                    path = DragAndDrop.paths[0];
+                    Debug.Log(path);
+                }
             }
         }
     }
-
-    
 }
