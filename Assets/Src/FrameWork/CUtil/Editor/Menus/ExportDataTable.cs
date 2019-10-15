a55e0ff4f48dbca1004c9774c66940e7a0d263b5
ricashao <szc699.good@163.com>
2019-10-15 08:08:38 +0800

    1.测试拖拽区域

--- Assets/Src/FrameWork/CUtil/Editor/Menus/ExportDataTable.cs
+++ Assets/Src/FrameWork/CUtil/Editor/Menus/ExportDataTable.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-public static class ExportDataTable
+public class ExportDataTable: EditorWindow 
 {
-    private static string excelPath = Application.dataPath + "/Excels";
-
-    [MenuItem("Assets/DataTool/Gen Config Table")]
-    public static void onCreateConfigTable()
+    string path;
+    Rect rect;
+    [MenuItem("KCFramework/ExcelTool/Gen Data Table")]
+    static void Init()
     {
-        
+        EditorWindow.GetWindow(typeof(ExportDataTable));
     }
+ 
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("路径");
+        //获得一个长300的框
+        rect = EditorGUILayout.GetControlRect(GUILayout.Width(300));
+        //将上面的框作为文本输入框
+        path = EditorGUI.TextField(rect, path);
+
+        if ((Event.current.type == EventType.DragUpdated)
+            && rect.Contains(Event.current.mousePosition))
+        {
+            //改变鼠标的外表
+            DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+        }
+
+        //如果鼠标正在拖拽中或拖拽结束时，并且鼠标所在位置在文本输入框内
+        if ((Event.current.type == EventType.DragExited)
+            && rect.Contains(Event.current.mousePosition))
+        {
+            if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+            {
+                path = DragAndDrop.paths[0];
+                Debug.Log(path);
+            }
+        }
+    }
+
+    
 }
