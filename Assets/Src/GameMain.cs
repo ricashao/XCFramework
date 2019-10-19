8ae8f69f62c3a0e797371dc96d97da56cd97e685
shaozhoucheng <shao.zhou.cheng@163.com>
2019-10-19 21:55:34 +0800

    1.协程管理器 2.音效管理器

--- Assets/Src/GameMain.cs
+++ Assets/Src/GameMain.cs
@@ -37,4 +37,33 @@ public class GameMain : BaseLua
 
 //        LuaScriptMgr.Instance.CallLuaFunction("TouchProxy.SetTouch", inputTouch);
     }
+
+    /// <summary>
+    /// 所有的update都走这边
+    /// </summary>
+    public void Update()
+    {
+        var delta = Time.deltaTime;
+        CoroutineManager.Instance.Tick(delta);
+        AudioManager.Instance.Tick(delta);
+        LuaScriptMgr.Instance.CallLuaFunction("GameMain.Tick", delta);
+    }
+    
+    void LateUpdate()
+    {
+        LuaScriptMgr.Instance.CallLuaFunction("GameMain.LateTick", Time.deltaTime);
+    }
+    
+    void OnApplicationQuit()
+    {
+        
+        Caching.ClearCache();
+#if UNITY_STANDALONE_WIN
+        //退出时，关闭ldt连接
+//        if (ioo.gameMain != null && ioo.gameMain.xluaMgr != null)//此时umgr有可能是空值
+//        {
+//            ioo.gameMain.xluaMgr.CallLuaFunction("luadebugger.Close");
+//        }
+#endif
+    }
 }
